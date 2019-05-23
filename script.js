d39f36f87eb203dfae70912580c40a675eda73c6
Matt Ziperman <alfie24@live.com>
2019-05-22 20:33:27 -0400

    Add clock and fix a bug when quitting
    If the Quit Game button was pressed before one second had passed
    the game would not be able to clear the interval potentially allowing
    multiple game clocks to run at the same time.  This fix allows the
    interval to be accessed outside of the countDown function allowing all
    intervals to be cleared upon quitting a game.

--- script.js
+++ script.js
@@ -1,82 +1,56 @@
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// set up the custom cursor
+// $(document).mousemove(e => {
+// 	$('.cursor').attr("style", `top: ${+e.pageY - 5}px; left: ${+e.pageX -5}px;`);
+// });
+
 // loading the default DOM values
 $( document ).ready(function() {
-	$('.prev-score').text(padDigits(score));
-	$('.high-score').text(padDigits(hiScore));
-	if ($(window).height() < 400){
-		showWarningScreen();
-	} else if ($(window).width() < 800){	
-		smallGame();
+	$('.prev-score').text('0');
+	$('.high-score').text(`${hiScore}`);
+	if (($(window).width() < 800) || $(window).height() < 600){	
+		renderWarning();
 	}	
 });
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// game variables and event listeners 
+// game code begins
 var score = 0;
 var hiScore = 0;
 var gameScore = [];
+var playedYet = false;
 var inGame = false;
 var intervals = [];
-var gameX = 750;
-var gameY = 450;
-var gamePadding = 25;
-var isSmall = false;
-var warning = false;
-var targetWidth = 30;
 
 $('#start-btn').click(e => {
-	getReady();
+	startGame();
 });
 
 $('#stop-btn').click(e => {
 	endGame();
 })
 
-$('.target').click(function(e) {
-	moveTarget();
-	updateScores();
-});
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// game functionality
-
-function getReady(){
-	inGame = true;
-
-	showPlayingScreen();
-	showReadyScreen();
-	$('#countdown').text(3)
-	readyCount(2);
-}
-
 function startGame(){
+	inGame = true;
+	showPlayingScreen();
+	moveTarget();
+	$('.current-score').text(`0`);
 	score = 0;
 	gameScore = [];	
-
-	hideReadyScreen();
-	centerTarget();
-	$('.current-score').text(padDigits(score));
+	
+	if(!playedYet){
+		$('.target').click(function(e) {
+			moveTarget();
+			updateScores();
+		});
+		playedYet = true;
+	}
 	$('.game-clock').text(15);
-	countDown(14);
-}
-
-function readyCount(seconds) {
-	var intervalId = setInterval(function() {
-		if (seconds > 0) {
-			$('#countdown').text(seconds);
-			seconds--;
-		} else {
-			clearInterval(intervalId);
-			startGame()
-		}
-	}, 1000);
-	intervals.push(intervalId);
+	countDown(14, 1, '.game-clock');
 }
 
 function countDown(seconds) {
 	var intervalId = setInterval(function() {
 		if (seconds >= 0) {
-			$('.game-clock').text(padDigits(seconds));
+			$('.game-clock').text(seconds);
 			seconds--;
 		} else {
 			clearInterval(intervalId);
@@ -88,129 +62,80 @@ function countDown(seconds) {
 		// fixes a bug that didn't clear the interval if quit before one second has passed.
 }
 
-function centerTarget(){
-	$('.target').attr('style', `transform: translate(
-		${gameX/2 + gamePadding - targetWidth/2}px, 
-		${gameY/2 + gamePadding - targetWidth/2}px)`
-	);
+function updateScores(){
+	gameScore.push('x');
+		$('.current-score').text(`${gameScore.length}`);
+		score = gameScore.length;
+		if(score > hiScore){
+			hiScore = gameScore.length;
+		}
 }
 
 function moveTarget(){
-	let x = (Math.floor(Math.random() * gameX) + gamePadding);
-	let y = (Math.floor(Math.random() * gameY) + gamePadding);
+	let x = (Math.floor(Math.random() * 750) + 25);
+	let y = (Math.floor(Math.random() * 450) + 25);
 	$('.target').attr('style', `transform: translate(${x}px, ${y}px)`);
 }
 
-function updateScores(){
-	gameScore.push('x');
-	$('.current-score').text(`${gameScore.length.toString().padStart(2, "0")}`);
-	score = gameScore.length;
-	if(score > hiScore){
-		hiScore = gameScore.length;
-	}
-}
-
 function endGame(){
-	inGame = false;
-	// clear any existing intervals, then remove all of them.
 	intervals.forEach(x => clearInterval(x));
 	intervals = [];
-
+		// clear any existing intervals, then remove all of them.
+	inGame = false;
 	showStartScreen();
-	$('.prev-score').text(padDigits(score));
-	$('.high-score').text(padDigits(hiScore));
-	if(score > 0) {
-		wellDone()
-	} else {
-		welcome();
-	}
-}
-
-function padDigits(num){
-	return num.toString().padStart(2, "0");
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// screen rendering
-function welcome() {
-	$('#eye').attr('style', 'display: block');
-	$('#wink').attr('style', 'display: none')
-	$('#greeting').text('Welcome!');
-}
-
-function wellDone() {
-	$('#eye').attr('style', 'display: none');
-	$('#wink').attr('style', 'display: block')
 	$('#greeting').text('Well done!');
+	$('.prev-score').text(`${score}`);
+	$('.high-score').text(`${hiScore}`);
 }
 
 function showStartScreen(){
-	warning = false;
-	$('.start-screen').attr('style', 'display: flex');
-	$('.game-screen').attr('style', 'display: none');
-	$('.error-screen').attr('style', 'display: none');
+	$('.game-display').attr('style', 'display: none');
+	$('.game').attr('style', 'display: none');
+	$('.game-screen').attr('style', 'display: none')
+	$('#smile').attr('style', 'display: block');
+	$('.start-menu').attr('style', 'display: flex');
 }
 
 function showPlayingScreen(){
-	$('.start-screen').attr('style', 'display: none');
-	$('.game-screen').attr('style', 'display: flex')
+	$('.game-display').attr('style', 'display: flex');
+	$('.game').attr('style', 'display: block');
+	$('.game-screen').attr('style', 'display: block')
+	$('#smile').attr('style', 'display: none');
+	$('.start-menu').attr('style', 'display: none');
 }
 
-function showWarningScreen(){
-	warning = true;
-	$('.start-screen').attr('style', 'display: none');
-	$('.game-screen').attr('style', 'display: none');
+$(window).resize(function(){
+	if (($(window).width() < 800) || $(window).height() < 600){	
+		endGame();
+		renderWarning();
+	}	
+	else if (($(window).width() >= 800) && $(window).height() >= 600){
+		renderContainer();
+	}
+});
+
+// screen sizing issues
+function renderWarning(){
+	$('.container').attr('style', 'display: none');
 	$('.error-screen').attr('style', 'display: flex');
 }
 
-function showReadyScreen(){
-	$('.get-ready').attr('style', 'display: flex');
-	$('.HUD').attr('style', 'display: none');
-	$('.target').attr('style', 'display: none');
-}
-
-function hideReadyScreen(){
-	$('.get-ready').attr('style', 'display: none');
-	$('.HUD').attr('style', 'display: flex');
-	$('.target').attr('style', 'display: block');
-}
-
-function smallGame(){
-	isSmall = true;
-	gameX = 290;
-	gameY = 260;
-	gamePadding = 20;
-	targetWidth = 16;
-}
-
-function normalGame(){
-	isSmall = false;
-	gameX = 750;
-	gameY = 450;
-	gamePadding = 25;
-	targetWidth = 30;
+function renderContainer(){
+	$('.container').attr('style', 'display: flex');
+	$('.error-screen').attr('style', 'display: none');
 }
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// handling screen size
-$(window).resize(function(){
-	if (($(window).height() < 400)) {
-		endGame();
-		showWarningScreen();
-	} else {
-		if (warning) {
-			showStartScreen();
-		} else {
-			if (($(window).width() < 800) && !isSmall){	
-				endGame();
-				smallGame();
-				// showWarningScreen();
-			}	
-			else if (($(window).width() >= 800) && isSmall){
-				endGame();
-				normalGame();
-			}
-		}
-	}
+/*
+const cursor = document.querySelector('.cursor');
+const cursorFollow = document.querySelector('.cursor-follow');
+const playBtn = document.querySelector('#start-btn')
+
+document.addEventListener('mousemove', e=> {
+	cursor.setAttribute("style", `top: ${+e.pageY - 5}px; left: ${+e.pageX -5}px;`);
 });
+
+document.addEventListener('mousemove', e=> {
+	cursorFollow.setAttribute("style", `top: ${+e.pageY - 8}px; left: ${+e.pageX - 8}px;`);
+});
+*/
